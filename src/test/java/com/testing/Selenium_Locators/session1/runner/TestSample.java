1899d3c985654e7ce41649fe7761a88290643c25
sakthi <sakthipalanisamy7@gmail.com>
2025-12-02 10:27:09 +0530

    add : Selenium Locators session 2

---
+++ src/test/java/com/testing/Selenium_Locators/session1/runner/TestSample.java
@@ -0,0 +1,5 @@
+package com.testing.Selenium_Locators.session1.runner;
+
+public class TestSample {
+
+}
---
+++ src/test/java/com/testing/Selenium_Locators/session2/utils/EventHandler.java
@@ -0,0 +1,7 @@
+package utils;
+
+import org.openqa.selenium.support.events.WebDriverListener;
+
+public class EventHandler implements WebDriverListener {
+
+}
